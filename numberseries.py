46575b7e4f2aa444deadac410a8e8f49c62762dc
EzhilarasanME <60417381+EzhilarasanME@users.noreply.github.com>
2020-01-29 15:18:51 +0530

    Create numberseries.py

---
+++ numberseries.py
@@ -0,0 +1,10 @@
+n= int(input("How many terms? "))
+a1, a2 = 0, 1
+count = 0
+print("Fibonacci sequence:")
+while count < n:
+    print(a1)
+    nth = a1 + a2
+    a1 = a2
+    a2 = nth
+    count +
